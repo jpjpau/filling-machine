184f79e8e34e0b233f96dcae839cc4b68436b55b
jpjpau <115387525+jpjpau@users.noreply.github.com>
2025-05-19 15:42:27 +0800

    v

--- filling-machine/valve_test.py
+++ filling-machine/valve_test.py
@@ -18,7 +18,7 @@ def main():
     instrument.serial.bytesize = 8
     instrument.serial.stopbits = 1
     instrument.clear_buffers_before_each_transaction = True
-    instrument.close_port_after_each_call = True
+    instrument.close_port_after_each_call = False
 
     try:
         # Open left valve (coil 1)
@@ -29,19 +29,23 @@ def main():
 
         # Close left valve
         #instrument.write_register(0x0000, 0x0000)
+        instrument.write_bit(0, 0)
         instrument.write_bit(1, 1)
         logger.info("Left valve closed")
         time.sleep(1)
 
         # Open right valve (coil 2)
         #instrument.write_register(0x0001, 0xFF00)
-        instrument.write_bit(2, 1)
+        #instrument.write_bit(2, 1)
         logger.info("Right valve opened")
         time.sleep(1)
 
         # Close right valve
         #instrument.write_register(0x0001, 0x0000)
         logger.info("Right valve closed")
+        
+        if instrument.serial.is_open:
+            instrument.serial.close()
 
     except Exception as e:
         logger.error("Modbus error during valve cycle", exc_info=e)
