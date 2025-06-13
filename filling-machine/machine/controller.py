2dcba85e992c2358461b156f72a9181f7fed64e5
jpjpau <115387525+jpjpau@users.noreply.github.com>
2025-06-13 17:12:57 +0800

    but

--- filling-machine/machine/controller.py
+++ filling-machine/machine/controller.py
@@ -15,7 +15,7 @@ from machine.mqtt_client import MqttClient
 
 import gpiod
 
-chip = gpiod.Chip('gpiochip1')
+chip = gpiod.Chip('gpiochip0')
 left_button_line = chip.get_line(17)
 right_button_line = chip.get_line(27)
 
